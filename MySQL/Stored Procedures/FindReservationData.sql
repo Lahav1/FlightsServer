81d25e668a0ae3a7b303a380aa6c686fe07da8bf
Omer Lavie <60101135+omerlavie@users.noreply.github.com>
2021-01-10 17:31:50 +0200

    Update FindReservationData.sql

--- MySQL/Stored Procedures/FindReservationData.sql
+++ MySQL/Stored Procedures/FindReservationData.sql
@@ -1,6 +1,7 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `FindReservationData`(reservation_id VARCHAR(45))
 BEGIN
-	SELECT MIN(DATE_ADD(flight.departure_time_GMT, INTERVAL source_airport.timezone HOUR)) AS local_departure_time,
+	SELECT full_name AS full_name,
+    MIN(DATE_ADD(flight.departure_time_GMT, INTERVAL source_airport.timezone HOUR)) AS local_departure_time,
 	CONCAT(source_airport.city, ", ", source_airport.country) AS departure_airport,
 	CONCAT(destination_airport.city, ", ", destination_airport.country) AS destination_airport,
     CONCAT(airline.IATA, SUBSTR(route.id, 2)) AS flight_number,
@@ -10,5 +11,6 @@ BEGIN
     LEFT JOIN airline ON(route.airline=airline.id)
 	LEFT JOIN airport AS source_airport ON(route.source_airport=source_airport.id)
 	LEFT JOIN airport AS destination_airport ON(route.destination_airport=destination_airport.id)
+    LEFT JOIN user ON(user.email=reservation.user)
 	WHERE reservation.id=reservation_id;
 END
